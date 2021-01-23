7840fcd735e9c409f14d06b0d31d27d196576192
jlsajfj <jlsajfj@hotmail.com>
2021-01-23 14:42:38 -0500

    getting it to wor ka bit more

--- optimizer/optimizer.py
+++ optimizer/optimizer.py
@@ -5,6 +5,14 @@ r = remote('45.134.3.200', 9660, level = 'debug')
 
 r.recvline()
 r.recvline()
+
 l = len(set(json.loads(r.recvline())))
 r.sendline(str(2**l-1).encode())
+r.recvuntil(' ')
+
+l = len(set(json.loads(r.recvline())))
+r.sendline(str(2**l-1).encode())
+r.recvuntil(' ')
 r.recvline()
+
+r.close()
