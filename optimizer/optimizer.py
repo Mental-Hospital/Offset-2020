370ff83fabb8c98dbebdae61303f4fa652056fc7
jlsajfj <jlsajfj@hotmail.com>
2021-01-23 14:22:48 -0500

    optimizer

--- optimizer/optimizer.py
+++ optimizer/optimizer.py
@@ -1,3 +1,10 @@
 from pwn import *
+import json, codecs
 
 r = remote('45.134.3.200', 9660, level = 'debug')
+
+r.recvline()
+r.recvline()
+l = len(set(json.loads(r.recvline())))
+r.sendline(str(2**l-1).encode())
+r.recvline()
