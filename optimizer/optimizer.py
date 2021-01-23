7bd82bc3c8ba9abaf7b78c1f760a82290069bf98
jlsajfj <jlsajfj@hotmail.com>
2021-01-23 15:05:38 -0500

    added while loop end statment

--- optimizer/optimizer.py
+++ optimizer/optimizer.py
@@ -6,14 +6,17 @@ r = remote('45.134.3.200', 9660, level = 'debug')
 r.recvline()
 r.recvline()
 
-while(True):
-	l = len(json.loads(r.recvline()))
-	r.sendline(str(2**l-1).encode())
-	r.recvuntil(' ')
+i = r.recvline()
 
-	# l = len(json.loads(r.recvline()))
-	# r.sendline(str(2**l-1).encode())
-	# r.recvuntil(' ')
-	# r.recvline()
+while(not i.startswith(b"level 2")):
+ l = len(json.loads(i))
+ r.sendline(str(2**l-1).encode())
+ r.recvuntil(' ')
+ i = r.recvline()
+
+ # l = len(json.loads(r.recvline()))
+ # r.sendline(str(2**l-1).encode())
+ # r.recvuntil(' ')
+ # r.recvline()
 
 r.close()
