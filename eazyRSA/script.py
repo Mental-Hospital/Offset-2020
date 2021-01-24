106d7964d8a167122604119815c882273a402121
Jason Liu <liu.jason84@gmail.com>
2021-01-24 18:06:05 -0500

    attempt 1 at bruteforce

--- eazyRSA/script.py
+++ eazyRSA/script.py
@@ -1,3 +1,5 @@
+import gmpy
+
 c = 3708354049649318175189820619077599798890688075815858391284996256924308912935262733471980964003143534200740113874286537588889431819703343015872364443921848
 e = 16
 p = 75000325607193724293694446403116223058337764961074929316352803137087536131383
@@ -7,9 +9,28 @@ n = 5203226874048586660123187369475542584442690830335849146139399959453354953925
 
 def decrypt(p, q, e, c):
  n = p*q
- phi = (p-1) * (q-1)
- d = pow(e, -1, phi)
- pti = pow(c, d, n)
- return codecs.decode(hex(pti)[2:],'hex').decode()
+ count = 0
+ max_i = 0
+ candidate_found = False
+ candidate = gmpy.mpz(c)
+ while not candidate_found:
+  candidate_found = True
+  for i in range(0,4):
+   if not gmpy.is_square(candidate):
+    print("failed at ", i)
+    if i > max_i:
+     max_i = i
+    print("max_i: ", max_i)
+    print(candidate)
+    candidate_found = False
+    break
+  if candidate_found:
+   break
+  count+=1
+  print("Trying count = ", count)
+  candidate += n
  
+ candidate_root = candidate.root(16)[0]
+ 
+ return candidate_root
 print(decrypt(p, q, e, c))
